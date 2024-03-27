c80a4c119fbb586f577777d58e63ef195d1cdc5f
Julien QUENTIN <jqntn88@gmail.com>
2024-03-27 21:26:01 +0100

    add correct if editor

--- Assets/Scripts/GIS/SceneViewScaler.cs
+++ Assets/Scripts/GIS/SceneViewScaler.cs
@@ -4,19 +4,19 @@ namespace Zero.GIS
 {
     internal sealed class SceneViewScaler : MonoBehaviour
     {
+#if UNITY_EDITOR
         [Range(1e-09f, 1.0f)]
         [SerializeField] private float _scale = 1e-05f;
 
-        private void Awake()
-        {
-            transform.localScale = Vector3.one;
-        }
-
-#if UNITY_EDITOR
         private void OnValidate()
         {
             transform.localScale = Vector3.one * _scale;
         }
 #endif
+
+        private void Awake()
+        {
+            transform.localScale = Vector3.one;
+        }
     }
 }
